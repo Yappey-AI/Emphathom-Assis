6bc499c09e7cbbfb9efdb1f8e5b3b1d696fe6e6f
Irfan <yetiie.os01@gmail.com>
2026-08-13 23:34:02 +0500

    Lecture functions and practice

--- hello.py
+++ hello.py
@@ -1,8 +1,10 @@
 
 print("Hello, I am Emphathom")
+x = input("Leme know your name: ").strip().capitalize()
 
 #What we doing?
-a = input("What are we up to today? ").strip().capitalize()
+a = input(f"Aight, {x}, what are we up to today? ").strip().capitalize()
+
 if a == "Nothing":
     print("What's wrong bro, You tired or something?")
 elif a == "Study" or a == "Studying":
@@ -10,4 +12,4 @@ elif a == "Study" or a == "Studying":
 elif a == "Work" or a == "Coding":
     print("Oh, So i don't have memory rn, Leme know if we have previosuly tlaked about it or if we are starting fresh.")
 else:
-    print(f"Aight, I'll find info on {a}.")
+    print(f"Aight, I'll find info on {a} for you.")
